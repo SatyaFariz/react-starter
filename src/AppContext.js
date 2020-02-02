a53bd3128a39fa06ff40c2ae5655e739f6da733d
satyafariz <satyafariznur@gmail.com>
2020-02-03 01:18:08 +0700

    routing setup

---
+++ src/AppContext.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/*
+const ApplicationContext = React.createContext({
+  fetch: () => {
+    throw new Error('Fetch method not initialized.');
+  },
+});
+
+export default ApplicationContext;*/
+
+const AppContext = React.createContext()
+
+export default AppContext
