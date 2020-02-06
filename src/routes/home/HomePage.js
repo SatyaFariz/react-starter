18cb096a6352e69ceeb25b35067455221bc6d340
satyafariz <satyafariznur@gmail.com>
2020-02-07 02:38:03 +0700

    use grid list

--- src/routes/home/HomePage.js
+++ src/routes/home/HomePage.js
@@ -12,8 +12,10 @@ const useStyles = makeStyles({
     paddingRight: 200,
     paddingTop: 20
   },
-  paper: {
-    borderRadius: 1
+  list: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr',
+    gridGap: '15px'
   }
 })
 
@@ -22,11 +24,36 @@ const Component = () => {
   return (
     <div className={c.container}>
     
-     
+      <div className={c.list}>
       <ProductItem 
         image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
         name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
       />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      <ProductItem 
+        image="https://ae01.alicdn.com/kf/Hd2a390b742f24cc2a96535ad7f43a3bat.jpg_200x200.jpg_.webp"
+        name="CHOETECH 100W GaN Fast Dual USB C Charger PD 3.0 For iPhone 11 Pro Max Support AFC FCP QC 3.0 for Samsung S10 Huawei P30 Xiaomi"
+      />
+      </div>
 
     </div>
   )
