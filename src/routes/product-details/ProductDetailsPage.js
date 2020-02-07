f6b1df182cc8c29bff493684196d22d134ea6aa7
satyafariz <satyafariznur@gmail.com>
2020-02-08 04:18:33 +0700

    use react-image-magnify

--- src/routes/product-details/ProductDetailsPage.js
+++ src/routes/product-details/ProductDetailsPage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import ProductImages from './ProductImages'
+
+const useStyles = makeStyles({
+  container: {
+    padding: 15
+  }
+})
 
 const Component = props => {
+  const c = useStyles()
   return (
-    <div>
-      Product Detail
+    <div className={c.container}>
+      <ProductImages/>
     </div>
   )
 }
---
+++ src/routes/product-details/ProductImages.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import ReactImageMagnify from 'react-image-magnify'
+
+const useStyles = makeStyles({
+  container: {
+
+  },
+  bigImage: {
+ /*   width: 450,
+    height: 450,*/
+    objectFit: 'cover'
+  }
+})
+
+const Component = props => {
+  const c = useStyles()
+  return (
+    <div className={c.container}>
+    {/*}  <img 
+        className={c.bigImage}
+        src="https://img.tek.id/img/content/2019/06/27/17621/apple-siapkan-macbook-pro-generasi-baru-siap-meluncur-september-UIL32am9YD.jpg"
+      
+      />*/}
+
+      <ReactImageMagnify {...{
+          imageClassName: c.bigImage,
+          enlargedImageClassName: c.bigImage,
+          smallImage: {
+              alt: 'Wristwatch by Ted Baker London',
+              width: 450,
+              height: 450,
+              src: 'https://img.tek.id/img/content/2019/06/27/17621/apple-siapkan-macbook-pro-generasi-baru-siap-meluncur-september-UIL32am9YD.jpg'
+          },
+          largeImage: {
+              src: 'https://img.tek.id/img/content/2019/06/27/17621/apple-siapkan-macbook-pro-generasi-baru-siap-meluncur-september-UIL32am9YD.jpg',
+              width: 500,
+              height: 500
+          }
+      }} />
+    </div>
+  )
+}
+
+export default Component
